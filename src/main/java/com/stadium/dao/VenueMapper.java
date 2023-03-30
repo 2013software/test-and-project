b47ceb0e733b3259bfa730a3cfa9a8b027ad21dd
“mzxtx” <you@example.com>
2023-03-30 10:32:02 +0800

    Delete useless function 'Venue getVenue(@Param("venue_name") String venue_name, @Param("state") String state); '

--- src/main/java/com/stadium/dao/VenueMapper.java
+++ src/main/java/com/stadium/dao/VenueMapper.java
@@ -13,14 +13,8 @@ public interface VenueMapper {
             @Result(column = "venue_name", property = "venue_name"),
             @Result(column = "useid", property = "userid"),
     })
-//    @Select("select * from venue_state, venue, time where venue_state.venueid = venue.id and venue_state.timeid = time.id")
-//    @Select("select * from venue")
-//    List<Venue> getVenueList_act();
-//    Venue getVenue(@Param("venue_name") String venue_name, @Param("state") String state);
-
-//
-//    @Select("select * from venue, venue_state, user, time where venue_state.timeid = time.id and venue_state.venueid = venue.id and venue_state.useid = user.userid and ")
-//    User getUser(@Param("username") String username, @Param("password") String password, @Param("identity") String identity);
+    @Select("select * from venue")
+    List<Venue> getVenueList_act();
 
 
     @Update("update venue_state set useid = #{userid}, state = 0 where state_id = #{id}")
